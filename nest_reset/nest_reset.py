19a6f929410d5751ac431ff254dc46d1cc97e1b1
viseshrp <viseshrprasad@gmail.com>
2019-01-11 21:57:20 -0500

    check for auth

--- nest_reset/nest_reset.py
+++ nest_reset/nest_reset.py
@@ -10,9 +10,12 @@ import nest
 
 def run(temp, client_id, client_secret):
     napi = nest.Nest(client_id=client_id, client_secret=client_secret)
-    click.launch(napi.authorize_url)
-    pin = click.prompt("Please enter your PIN to continue", hide_input=True, type=str)
-    napi.request_token(pin=pin)
+
+    if napi.authorization_required:
+        click.launch(napi.authorize_url)
+        pin = click.prompt("Please enter your PIN to continue", hide_input=True, type=str)
+        napi.request_token(pin=pin)
+
     thermostat = napi.structures[0].thermostats[0]
 
     if thermostat.is_locked:
